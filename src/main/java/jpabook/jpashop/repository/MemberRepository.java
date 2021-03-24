5ad6aa95e77cd06fbf32902ff3408dc626981fab
LEESANGNAM <fgh0116@naver.com>
2021-03-24 20:04:02 +0900

    회원테스트

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,13 +9,14 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository// 자동으로 스프링빈으로 관리
-
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
-
+//    public MemberRepository(EntityManager em) {
+//        this.em = em;
+//    }  @RequiredArgsConstructor  로 대신생성
 
     public void save(Member member){
     em.persist(member);
