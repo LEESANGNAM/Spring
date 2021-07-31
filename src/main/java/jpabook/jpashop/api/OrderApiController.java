815fce57cb6733ab3672e85a5f5567ef2a00c272
LEESANGNAM <fgh0116@naver.com>
2021-08-01 00:22:11 +0900

    10.JPA2(API 컬렉션조회최적화V2)

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,13 +41,15 @@ public class OrderApiController {
                 .collect(Collectors.toList());
         return collect;
     }
+
+    @Getter
     static class OrderDto{
         private Long orderId;
         private String name;
         private LocalDateTime orderDate;
         private OrderStatus orderStatus;
         private Address address;
-        private List<OrderItem> orderItems;
+        private List<OrderItemDto> orderItems;
 
         public OrderDto(Order order){
             orderId = order.getId();
@@ -54,7 +57,23 @@ public class OrderApiController {
             orderDate = order.getOrderDate();
             orderStatus = order.getStatus();
             address = order.getDelivery().getAddress();
-            orderItems=order.getOrderItems();
+            order.getOrderItems().stream().forEach(o->o.getItem().getName());
+            orderItems=order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+        @Getter
+        static class OrderItemDto{
+            private String itemName;
+            private int orderPrice;
+            private int count;
+
+
+            public OrderItemDto(OrderItem orderItem) {
+                itemName = orderItem.getItem().getName();
+                orderPrice=orderItem.getOrderPrice();
+                count= orderItem.getCount();
+            }
         }
 
     }
